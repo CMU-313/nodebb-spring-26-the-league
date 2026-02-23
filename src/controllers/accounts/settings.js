1d0ae15d2b05b54018847356b113b1f89f3b433c
sittingthyme <nicholan@andrew.cmu.edu>
2026-02-23 18:57:50 -0500

    Change default(custom) to default and reomve duplicate custom option

--- src/controllers/accounts/settings.js
+++ src/controllers/accounts/settings.js
@@ -231,11 +231,9 @@ async function getHomePageRoutes(userData) {
 }
 
 async function getSkinOptions(userData) {
-	const defaultSkin = _.capitalize(meta.config.bootswatchSkin) || '[[user:no-skin]]';
 	const bootswatchSkinOptions = [
 		{ name: '[[user:no-skin]]', value: 'noskin' },
-		{ name: `[[user:default, ${defaultSkin}]]`, value: '' },
-		{ name: '[[user:custom-skin-colors]]', value: 'custom' },
+		{ name: 'Default', value: '' },
 	];
 	const customSkins = await meta.settings.get('custom-skins');
 	if (customSkins && Array.isArray(customSkins['custom-skin-list'])) {
